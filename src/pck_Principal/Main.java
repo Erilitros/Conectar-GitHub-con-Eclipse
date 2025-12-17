21e640611edef80851e4acb8fd67407303028820
Eric Rocamora <ericrocarubio@gmail.com>
2025-12-17 19:14:57 +0100

    Refactor output in Main.java

--- src/pck_Principal/Main.java
+++ src/pck_Principal/Main.java
@@ -6,8 +6,9 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola Mundo");
-		System.out.println("Eric chupa pitos");
+		System.out.println("Eric");
 		System.out.println("Hola Mundo");
 	}
 
 }
+
